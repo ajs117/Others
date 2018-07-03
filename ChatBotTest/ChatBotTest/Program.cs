e3f85f8c123eefb2a8c4dd68539160443355b053
Alexander Smart <Alexander.Smart@ukics.com>
2018-07-03 09:48:25 +0100

    Test chat bot

---
+++ ChatBotTest/ChatBotTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using ApiAiSDK;
+using System;
+
+namespace ChatBotTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = new AIConfiguration("eb3389c5a99b41bdb080a9ec2c9ebe98", SupportedLanguage.English);
+            var apiAi = new ApiAi(config);
+
+            Console.WriteLine("Welcome to chat bot test, please say something to the bot.");
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                var response = apiAi.TextRequest(command);
+                Console.WriteLine(response.Result.Fulfillment.Speech);
+            }
+        }
+    }
+}
